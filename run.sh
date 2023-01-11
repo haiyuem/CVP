008dc4196a4a6b5767c90854cc80249ee0819157
haiyuem <ameliama723@163.com>
2023-01-10 22:28:29 -0500

    few changes

---
+++ run.sh
@@ -0,0 +1,19 @@
+#sketch
+# ./cvp /home/haiyue/research/value_prediction/sketch/trace_sketch_10x200 > sketch_10x200_base.log
+# ./cvp -v -p -t 2 /home/haiyue/research/value_prediction/sketch/trace_sketch_10x200 > sketch_10x200_perfect.log
+# ./cvp -v -t 2 /home/haiyue/research/value_prediction/sketch/trace_sketch_10x200 > sketch_10x200_predict_winnerpred.log
+# ./cvp /home/haiyue/research/value_prediction/sketch/trace_sketch_unroll_10x200 > sketch_unroll_10x200_base.log
+# ./cvp -v -p -t 2 /home/haiyue/research/value_prediction/sketch/trace_sketch_unroll_10x200 > sketch_unroll_10x200_perfect.log
+
+# ./cvp /home/haiyue/research/value_prediction/sketch/trace_sketch_diffval_100x20 > sketch_diffval_100x20_base.log
+# ./cvp -v -p -t 2 /home/haiyue/research/value_prediction/sketch/trace_sketch_diffval_100x20 > sketch_diffval_100x20_perfect.log
+# ./cvp -v -t 2 /home/haiyue/research/value_prediction/sketch/trace_sketch_diffval_100x20 > sketch_diffval_100x20_predict_winnerpred.log
+
+#mjpeg
+# ./cvp /home/haiyue/research/value_prediction/mjpegtools/image/lane_detection/trace_mpeg2enc_1img_libmpeg2encpp > mpeg2enc_1img_libmpeg2encpp_base.log
+# ./cvp -v -p -t 2 /home/haiyue/research/value_prediction/mjpegtools/image/lane_detection/trace_mpeg2enc_1img_libmpeg2encpp > mpeg2enc_1img_libmpeg2encpp_perfect.log
+# ./cvp -v -t 2 /home/haiyue/research/value_prediction/mjpegtools/image/lane_detection/trace_mpeg2enc_1img_libmpeg2encpp > mpeg2enc_1img_libmpeg2encpp_predict.log
+
+#Lane detection
+# ./cvp /home/haiyue/research/ultra_fast_lane_detection/lane_detect_convert/caffe_lane_detection/pin/trace_lane_det_ATL_spNBmm_b1 > lane_det_ATL_spNBmm_b1_base.log
+# ./cvp -v -p -t 2 /home/haiyue/research/ultra_fast_lane_detection/lane_detect_convert/caffe_lane_detection/pin/trace_lane_det_ATL_spNBmm_b1 > lane_det_ATL_spNBmm_b1_perfect.log
